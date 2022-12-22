eab8338e6d69e1265b0bb9cc0d9d5d4aba63f1b6
Kacperski2115 <kacperski2512@gmail.com>
2022-12-22 21:15:53 +0100

    Final commit

--- app/src/main/java/com/example/zadaniekalendarz/MainActivity.kt
+++ app/src/main/java/com/example/zadaniekalendarz/MainActivity.kt
@@ -4,65 +4,66 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.time.LocalDate
-import java.time.ZoneId
 import java.util.*
+import java.util.Calendar
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
 
 @Suppress("DEPRECATION")
+var StartDate : Long=1
+var EndDate : Long=1
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val calendar = findViewById<CalendarView>(R.id.calendar)
-        val startbutton = findViewById<Button>(R.id.StartDateButton)
-        val endbutton = findViewById<Button>(R.id.EndDateButton)
+        val startbutton = findViewById<Button>(R.id.StartButton)
         val startshow = findViewById<TextView>(R.id.StartDateText)
         val endshow = findViewById<TextView>(R.id.EndDateText)
-        val show = findViewById<TextView>(R.id.show)
-        val showbutton=findViewById<Button>(R.id.ShowButton)
-
-        calendar.minDate = System.currentTimeMillis()
-        calendar.maxDate = LocalDate.now().plusYears(2).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
-        calendar.setFirstDayOfWeek(Calendar.MONDAY);
-
-        val startdate = mutableListOf<Int>(0,0,0)
-        val enddate = mutableListOf<Int>(0,0,0)
-        val data = arrayListOf<Int>(0,0,0)
-
-        calendar.setOnDateChangeListener(){CalendarView, dzien, mies, rok ->
-            data[0] = dzien
-            data[1] = mies +1
-            data[2] = rok
-        }
+        val Length = findViewById<TextView>(R.id.Length)
+        val LengthButton=findViewById<Button>(R.id.LengthButton)
+        val calendar=Calendar.getInstance()
+        val mindate= calendar.timeInMillis
+        val maxdate= calendar.timeInMillis + 63113904000
+        val constraints= CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointForward.now())
+            .setStart(mindate)
+            .setEnd(maxdate)
+            .build()
 
         startbutton.setOnClickListener {
-            startdate[0] = data[0]
-            startdate[1] = data[1]
-            startdate[2] = data[2]
-            startshow.text = "Data wyjazdu " +data[0].toString()+"-"+data[1].toString()+"-"+data[2].toString();
+            val calendar = MaterialDatePicker.Builder.dateRangePicker()
+                .setCalendarConstraints(constraints)
+                .setTitleText("Select dates")
+                .build()
+            calendar.show(supportFragmentManager, "Date_picker_tag")
+            calendar.addOnPositiveButtonClickListener { datePicked->
+                StartDate = datePicked.first
+                EndDate = datePicked.second
+                startshow.text="Data twojego wyjazdu to: "+ConvertDate(StartDate)
+                endshow.text="Data twojego przyjazdu to: "+ConvertDate(EndDate)
+            }
+            LengthButton.setOnClickListener {
+                val StartDateSub = StartDate/1000/60/60/24
+                val EndDateSub = EndDate/1000/60/60/24
+                val DatesSub = (EndDateSub-StartDateSub).toInt()
+                if(DatesSub==1){
+                    Length.text="Twój wyjazd będzie trwał " + DatesSub.toString() + " dzień"
+                }else  Length.text = "Twój wyjazd będzie trwał " + DatesSub.toString() + " dni"
+            }
         }
-        endbutton.setOnClickListener {
-            enddate[0] = data[0]
-            enddate[1] = data[1]
-            enddate[2] = data[2]
-            endshow.text =
-                "Data przyjazdu " + data[0].toString() + "-" + data[1].toString() + "-" + data[2].toString();
-        }
-            showbutton.setOnClickListener {
-                if(startdate[0] > enddate[0] || startdate[2] > enddate[2] && startdate[1] == enddate[1])
-                    show.text = "Nieprawidłowa data-Data wyjazdu musi być wcześniej niż data przyjazdu"
-                else {
-                    val dataendshow = (enddate[0] * 360) + (enddate[1] * 30) + enddate[2]
-                    val datastartshow = (startdate[0] * 360) + (startdate[1] * 30) + startdate[2]
-                    val datashow = dataendshow.toChar() - datastartshow.toChar()
-                    show.text = "Wyjazd trwa " + datashow.toString() + " dni"
-                }
-        }
+    }
+    private fun ConvertDate(date: Long) :String{
+        val datetoconvert=Date(date)
+        val format=SimpleDateFormat(
+            "dd-MM-yyyy"
+        )
+        return format.format(datetoconvert)
     }
 }
